0f828370c47c4a1e705883223a6a40ae0880f513
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 <stankevichuuladimiru@gmail.com>
2021-08-19 17:34:10 +0300

    fix: bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-
-exports.min = function min (array) {
-  if (array === 0)
+exports.min = function min (array) { 
+   if (array == 0 || !array)
   return 0;
-  
+array.sort((a, b) => a - b);
+return array[0];
 }
+
 
 exports.max = function max (array) {
+  if (array == 0 || !array)
   return 0;
-}
+  let number = array[0];
+    for (let i = 1; i < array.length - 1; i++){
+      if (number < array[i + 1]){
+       number = array[i + 1];
+      }
+    }
+  return number
+};
+
 
 exports.avg = function avg (array) {
+  if (array == 0 || !array)
   return 0;
+
+  let sum = +0;
+  let i = 0;
+ while (i < array.length){
+sum = sum + array[i];
+i = i + 1;
+ }
+ return sum / array.length;
 }
